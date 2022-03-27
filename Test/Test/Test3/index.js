0371d4d0c5e3b7152ce053153414b6e3ab8d727f
NAM <namxuan.nguyen1990@gmail.com>
2022-03-28 01:50:26 +0700

    Casehoanchinh

--- Test/Test/Test3/index.js
+++ Test/Test/Test3/index.js
@@ -4,22 +4,60 @@ let ctx = cvs.getContext("2d");
 let bird = new Image();
 let bg = new Image();
 let fg = new Image();
-let pipeNorth = new Image();
-let pipeSouth = new Image();
+let ongtren = new Image();
+let ongduoi = new Image();
 
-bird.src = "images/bird.png";
-bg.src = "images/bg.png";
-fg.src = "images/fg.png";
-pipeNorth.src = "images/ongtren.png";
-pipeSouth.src = "images/ongduoi.png";
+bird.src = "bird.png";
+bg.src = "bg.png";
+fg.src = "fg.png";
+ongtren.src = "ongtren.png";
+ongduoi.src = "ongduoi.png";
 
-let gap = 75;
-let constant = pipeNorth.height+gap;
+let gap = 320;
+let constant = ongtren.height+gap;
+
+let bX = 80;
+let bY = 350;
+let gravity = 2
+
+document.addEventListener("keydown",function (){
+    bY -= 120;
+})
+
+let ong = [];
+
+ong[0] = {
+    x : cvs.width,
+    y: 0
+}
 
 function draw() {
     ctx.drawImage(bg,0,0);
-    ctx.drawImage(pipeNorth,100,0);
-    ctx.drawImage(pipeSouth,100,0+constant)
+    for (let i=0; i<ong.length;i++) {
+        ctx.drawImage(ongtren,ong[i].x,ong[i].y)
+        ctx.drawImage(ongduoi,ong[i].x,ong[i].y+constant);
+        ong[i].x--;
+
+        if (ong[i].x == 300) {
+            ong.push({
+                x : cvs.width,
+                y : Math.floor(Math.random()*ongtren.height) - ongtren.height
+            })
+        }
+        if (bX + bird.width >= ong[i].x && bX <= ong[i].x + ongtren.width
+            && (bY <= ong[i].y + ongtren.height || bY+bird.height >=
+                ong[i].y+constant) || bY + bird.height >= cvs.height - fg.height) {
+            location.reload();
+        }
+    }
+
+    ctx.drawImage(fg,0,cvs.height - fg.height);
+    ctx.drawImage(bird,bX,bY);
+    bY += gravity;
+    requestAnimationFrame(draw)
+
 }
 
-draw()
+
+
+draw();
